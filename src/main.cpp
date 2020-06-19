5cdffc5e52c8499b2d46c16e0772e478fb03b1f2
balinth0rvath <horvath0balint@gmail.com>
2020-06-19 13:32:07 +0200

    Generate trajectory along the center line using spline

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,7 @@
 #include "Eigen-3.3/Eigen/Core"
 #include "Eigen-3.3/Eigen/QR"
 #include "helpers.h"
+#include "spline.h"
 #include "json.hpp"
 
 // for convenience
@@ -57,6 +58,8 @@ int main() {
     // "42" at the start of the message means there's a websocket message event.
     // The 4 signifies a websocket message
     // The 2 signifies a websocket event
+		int lane = 1;
+		double ref_vel = 48.5;
     if (length && length > 2 && data[0] == '4' && data[1] == '2') {
 
       auto s = hasData(data);
@@ -88,6 +91,7 @@ int main() {
           //   of the road.
           auto sensor_fusion = j[1]["sensor_fusion"];
 
+					int prev_size = previous_path_x.size();
           json msgJson;
 
           vector<double> next_x_vals;
@@ -97,13 +101,81 @@ int main() {
            * TODO: define a path made up of (x,y) points that the car will visit
            *   sequentially every .02 seconds
            */
+			
+					vector<double> ptsx;
+					vector<double> ptsy;
+
+					double ref_x = car_x;
+					double ref_y = car_y;
+					double ref_yaw = deg2rad(car_yaw);
+
+		
+					if (prev_size < 2)
+					{
+						double prev_car_x = car_x - cos(car_yaw);
+						double prev_car_y = car_y - cos(car_yaw);
+
+						ptsx.push_back(prev_car_x);
+						ptsx.push_back(car_x);
+
+						ptsy.push_back(prev_car_y);
+						ptsy.push_back(car_y);
+					} else
+					{
+						ref_x = previous_path_x[prev_size-1];
+						ref_y = previous_path_y[prev_size-1];
+	
+						double ref_x_prev = previous_path_x[prev_size-2];
+						double ref_y_prev = previous_path_y[prev_size-2];
+
+						ref_yaw = atan2(ref_y - ref_y_prev, ref_x - ref_x_prev);
+						ptsx.push_back(ref_x_prev);
+						ptsx.push_back(ref_x);
+
+						ptsx.push_back(ref_y_prev);
+						ptsx.push_back(ref_y);
+					}
+
+					vector<double> next_wp0 = getXY(car_s+30,(2+4*lane),map_waypoints_s,
+							map_waypoints_x, map_waypoints_y);
+					vector<double> next_wp1 = getXY(car_s+60,(2+4*lane),map_waypoints_s,
+							map_waypoints_x, map_waypoints_y);
+					vector<double> next_wp2 = getXY(car_s+90,(2+4*lane),map_waypoints_s,
+							map_waypoints_x, map_waypoints_y);
+
+					ptsx.push_back(next_wp0[0]);
+					ptsx.push_back(next_wp1[0]);
+					ptsx.push_back(next_wp2[0]);
+
+					ptsy.push_back(next_wp0[0]);
+					ptsy.push_back(next_wp1[0]);
+					ptsy.push_back(next_wp2[0]);
+
+					for(int i=0; i< ptsx.size(); ++i)
+					{
+						double shift_x = ptsx[i] - ref_x;
+						double shift_y = ptsy[i] - ref_y;
+
+						ptsx[i] = (shift_x * cos(0-ref_yaw) - shift_y * sin(0-ref_yaw));
+						ptsy[i] = (shift_x * sin(0-ref_yaw) - shift_y * cos(0-ref_yaw));
+					}
+
+					tk::spline s;
+
+					s.set_points(ptsx, ptsy);
+
+
+					for(int i=0; i<previous_path_x.size(); i++)
+					{
+						next_x_vals.push_back(previous_path_x[i]);
+						next_y_vals.push_back(previous_path_y[i]);
+					}
 
 
           msgJson["next_x"] = next_x_vals;
           msgJson["next_y"] = next_y_vals;
 
           auto msg = "42[\"control\","+ msgJson.dump()+"]";
-
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
         }  // end "telemetry" if
       } else {
